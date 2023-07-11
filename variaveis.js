afabeb66184a094ff9738518b579e78906d329ec
Lara Ayne <82737970+techlaradev@users.noreply.github.com>
2023-07-11 15:53:47 -0300

    Primeiro exercício

---
+++ variaveis.js
@@ -0,0 +1,17 @@
+//Primeiro trabalho com operadores e variáveis s2
+
+//VARIÁVEIS - literalmente uma "pastinha" que guarda as informações rs
+    //Declarando variáveis e mostrando na tela:
+    //let Age = 30;
+    //console.log("Talita tem:", Age, "anos");
+
+//Os Tipos de variáveis - por algum motivo muito obscuro o Javascript entende sem que a gente tenha que declarar a 
+//lindíssima da variável haha, só taca o Let que ele se vira!
+
+let age = 30;
+let lenth = 1.65;
+let name = 'Talita';
+let outra;
+
+console.log("Essa pessoa é:");
+console.log(name,"que possui", age, "anos, e", lenth );
