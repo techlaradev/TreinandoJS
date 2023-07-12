642961bcb6e0d1c14cd555a6764360351d3a3b9c
Lara Ayne <82737970+techlaradev@users.noreply.github.com>
2023-07-12 11:33:44 -0300

    Update operadores.js
    Testing some github functions to update some archives :)

--- operadores.js
+++ operadores.js
@@ -1,2 +1,30 @@
-//OPERAÇÕES ARITMÉTICAS
+/*OPERAÇÕES ARITMÉTICAS
+adição +
+subtração -
+exponenciação **
+divisão / 
+resto da divisão
+multiplicação * 
+*/
+    //Declarando variáveis, só que eu não quero ter uma boa prática rsrs
+    let A = 5; let B= 25;
+    let nameStudent = "Lara Jane"; //sou estudante, eu posso rsrs
+    const soma = A+B;
+    const sub = A-B;
+    const multi = A*B;
+    const div = A/B;
+    const expo = A**B;
+    const resto = B % 5;
+
+    console.log("A Soma desses números são:", soma);
+    console.log("O resultado daquela da substração entre esses números:",sub)
+    console.log("O produto e resulta em:",multi);
+    console.log("O Resultado da divisão é:",div);
+    console.log("e o grand finale!!! EXPONENCIAÇÃO:", expo);
+    console.log("o resto:",resto);
+
+/*Biblioteca né, pq se não nada funciona ahaha (MATH)
+e cara é maravilhoso pq é tudo embutido, eu n preciso importar nada! que emocionante!! */
+
+console.log("exibindo um valor de PI pq eu quero B)", Math.PI);
 
